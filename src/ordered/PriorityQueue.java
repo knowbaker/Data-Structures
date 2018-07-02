ea993e419d0b1fca72208ace353ba18cc4deb646
knowbaker <psingh.us@gmail.com>
2018-07-01 23:01:33 -0700

    Initial Commit

---
+++ src/ordered/PriorityQueue.java
@@ -0,0 +1,125 @@
+package ordered;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+public class PriorityQueue<E> {
+	private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+	private E[] pq;
+	private int n; //number of elements in the priority queue
+	private Comparator<E> comparator;
+	
+	@SuppressWarnings("unchecked")
+	public PriorityQueue(int initialCapacity) {
+		applyGreaterThanZeroCheck(initialCapacity);
+		this.pq = (E[])new Object[initialCapacity];
+	}
+	
+	@SuppressWarnings("unchecked")
+	public PriorityQueue(int initialCapacity, Comparator<E> comparator) {
+		applyGreaterThanZeroCheck(initialCapacity);
+		this.comparator = comparator;
+		this.pq = (E[])new Object[initialCapacity];
+	}
+	
+	public boolean isEmpty() {
+		return n == 0;
+	}
+	
+	public int size() {
+		return n;
+	}
+	
+	/**
+	 * Swap root element with last element, reduce heap size, and let the new root sink down
+	 * Return the original root element
+	 */
+	public E poll() {
+		if(n == 0)
+			throw new NoSuchElementException();
+		
+		E e = pq[0];
+		swap(0, n-- - 1);
+		sink(0);
+		return e;
+	}
+	
+	/**
+	 * 
+	 * @param e: Element to be added to the heap
+	 * Add element to the end and let it sink up
+	 */
+	public void add(E e) {
+		if(n == MAX_ARRAY_SIZE)
+			throw new ArrayIndexOutOfBoundsException("Priority queue size has reached maximum allowable array size");
+		
+		pq[n++] = e;
+		swim(n - 1);
+	}
+	
+	private void sink(int i) {
+		int x = getExtremeChildIndexOf(i);
+		if(x < 0 || compare(i, x) <= 0)
+			return;
+		
+		swap(i, x);
+		sink(x);
+	}
+	
+	private void swim(int i) {
+		int x = getParentIndexOf(i);
+		if(x < 0 || compare(x, i) <= 0)
+			return;
+		
+		swap(i, x);
+		swim(x);
+	}
+	
+	private int getParentIndexOf(int i) {
+		return (i - 1) >> 1;
+	}
+	
+	//Smallest according to comparator
+	private int getExtremeChildIndexOf(int i) {
+		int l = getLeftChildIndexOf(i);
+		int r = getRightChildIndexOf(i);
+		if(l < 0 && r < 0)
+			return -1;
+		if(l < 0)
+			return r;
+		if(r < 0)
+			return l;
+		return compare(l, r) < 0 ? l : r;
+	}
+	
+	private int getLeftChildIndexOf(int i) {
+		int l = (i << 1) + 1;
+		return l >= n || l < 0 ? -1 : l;
+	}
+	
+	private int getRightChildIndexOf(int i) {
+		int r = (i + 1) << 1;
+		return r >= n || r < 0 ? -1 : r;
+	}
+	
+	//The costly cast on every compare call can be eliminated if
+	//we define the generic E as E extends Comparable<E> in the class declaration
+	@SuppressWarnings("unchecked")
+	private int compare(int i, int j) {
+		if(comparator == null)
+			return ((Comparable<E>) pq[i]).compareTo(pq[j]);
+		else
+			return comparator.compare(pq[i], pq[j]);
+	}
+	
+	private void swap(int i, int j) {
+		E e = pq[i];
+		pq[i] = pq[j];
+		pq[j] = e;
+	}
+	
+	private void applyGreaterThanZeroCheck(int n) {
+		if(n <= 0)
+			throw new IllegalArgumentException("Provided value must be a positive integer");
+	}
+}
---
+++ test/ordered/PriorityQueueTest.java
@@ -0,0 +1,38 @@
+package ordered;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Comparator;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class PriorityQueueTest {
+
+	@Test
+	public void testMinHeapProperty() {
+		test(null);
+	}
+	
+	@Test
+	public void testMaxHeapProperty() {
+		test(Comparator.comparingInt(Integer::intValue).reversed());
+	}
+	
+	private void test(Comparator<Integer> c) {
+		int n = 1_000_000;
+		PriorityQueue<Integer> pq = c == null ? new PriorityQueue<>(n) : new PriorityQueue<>(n, c);
+		Integer[] a = new Integer[n];
+		for(int i = 0; i < n; i++) {
+			int x = (int) (Math.floor(Math.random() * Math.floor(5)) + 1);
+			pq.add(x);
+			a[i] = x;
+		}
+		Arrays.sort(a, c); //if c == null, it defaults to Arrays,sort(a)
+		for(int x : a) {
+			int actual = pq.poll().intValue();
+			assertEquals(x, actual);
+		}
+	}	
+}
