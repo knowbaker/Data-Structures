926144887e781a4e9150ca6d90142788e8bb0fe0
knowbaker <psingh.us@gmail.com>
2018-07-30 23:36:04 -0700

    An iterable Permutation generator for any string

---
+++ src/iterable/Permutation.java
@@ -0,0 +1,119 @@
+package iterable;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * An Iterable and Iterator for enumerating Permutations
+ * Example:
+ * 	Permutation perm = Permutation.of("atom")
+ * 	for(String x : perm)
+ * 		System.out.println(x);
+ * 
+ * Similarly, the iteration can be accomplished using the while(perm.hasNext()) loop
+ * 
+ * TODO: Enhance buffer to queue more elements and find an optimized number
+ * to be stored in the buffer
+ */
+public class Permutation implements Iterator<String>, Iterable<String> {	
+	private final BlockingQueue<char[]> dataQ;
+	private final BlockingQueue<Boolean> cmdQ;
+	private final Queue<String> buffer;
+	
+	private Permutation(String s, BlockingQueue<char[]> dataQ, BlockingQueue<Boolean> cmdQ) {
+		this.dataQ = dataQ;
+		this.cmdQ = cmdQ;
+		this.buffer = new ArrayDeque<>();
+		Runnable producer = new Producer(s.toCharArray(), dataQ, cmdQ);
+		new Thread(producer).start();
+		addToBuffer();
+	}
+	
+	public static Permutation of(String s) {
+		Objects.requireNonNull(s);
+		if(s.isEmpty())
+			throw new IllegalArgumentException("Empty String not allowed");
+		
+		BlockingQueue<char[]> dataQ = new ArrayBlockingQueue<>(1);
+		BlockingQueue<Boolean> cmdQ = new ArrayBlockingQueue<>(1);
+		Permutation perm = new Permutation(s, dataQ, cmdQ);
+		return perm;
+	}
+	
+	@Override
+	public Iterator<String> iterator() {
+		return this;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return !buffer.isEmpty();
+	}
+
+	@Override
+	public String next() {
+		addToBuffer();
+		return buffer.poll();
+	}
+	
+	private void addToBuffer() {
+		try {
+			cmdQ.put(true);
+			char[] a = dataQ.take();
+			if(a.length > 0)//a.length == 0 -> No more permutations remaining
+				buffer.add(String.valueOf(a));
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}		
+	}
+	
+	private class Producer implements Runnable {
+		private char[] a;
+		private final BlockingQueue<char[]> dataQ;
+		private final BlockingQueue<Boolean> cmdQ;
+		
+		Producer(char[] a, BlockingQueue<char[]> dataQ, BlockingQueue<Boolean> cmdQ) {
+			this.a = a;
+			this.dataQ = dataQ;
+			this.cmdQ = cmdQ;
+		}
+
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					cmdQ.take();
+					enumerate(0);
+					dataQ.put(new char[] {});//No more permutations remaining
+					break;
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		
+		private void enumerate(int depth) throws InterruptedException {
+			if(depth == a.length) {
+				dataQ.put(a);
+				cmdQ.take();
+				return;
+			}
+			
+			for(int i = depth; i < a.length; i++) {
+				swap(i, depth);
+				enumerate(depth + 1);
+				swap(i, depth);
+			}
+		}
+
+		private void swap(int x, int y) {
+			char c = a[x];
+			a[x] = a[y];
+			a[y] = c;
+		}
+	}
+}
---
+++ test/iterable/PermutationTest.java
@@ -0,0 +1,56 @@
+package iterable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class PermutationTest {
+
+	@Test
+	/*
+	 * If Permutation is Iterable, then it should be usable with the enhanced for loop
+	 */
+	public void testIterableFor() {
+		testFor(Permutation.of("x"), Arrays.asList("x"));
+		testFor(Permutation.of("abc"), Arrays.asList("abc", "acb", "bac", "bca", "cba", "cab"));
+	}
+	
+	@Test
+	/*
+	 * If Permutation is Iterable, then it should be usable with the for-each loop
+	 */
+	public void testIterableForEach() {
+		testForEach(Permutation.of("x"), Arrays.asList("x"));
+		testForEach(Permutation.of("abc"), Arrays.asList("abc", "acb", "bac", "bca", "cba", "cab"));
+	}
+	
+	@Test
+	/*
+	 * If Permutation is an Iterator, then it should be usable with the while(hasNext()) loop
+	 */
+	public void testIterator() {
+		testWhile(Permutation.of("x"), Arrays.asList("x"));
+		testWhile(Permutation.of("abc"), Arrays.asList("abc", "acb", "bac", "bca", "cba", "cab"));
+	}
+	
+	private void testFor(Permutation actualPerm, List<String> expectedPerm) {
+		int i = 0;
+		for(String actual : actualPerm)
+			assertEquals(expectedPerm.get(i++), actual);
+	}
+	
+	private void testForEach(Permutation actualPerm, List<String> expectedPerm) {
+		Iterator<String> iter = expectedPerm.iterator();
+		actualPerm.forEach(actual -> assertEquals(actual, iter.next()));
+	}
+
+	private void testWhile(Permutation actualPerm, List<String> expectedPerm) {
+		int i = 0;
+		while(actualPerm.hasNext())
+			assertEquals(expectedPerm.get(i++), actualPerm.next());
+	}
+}
